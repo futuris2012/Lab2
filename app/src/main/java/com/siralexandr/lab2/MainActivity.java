8c42e88baf602d464d23175c223e6d3e38dead97
futuris2012 <sania.ratushnyy@gmail.com>
2017-03-13 17:07:00 +0400

    почти

--- app/src/main/java/com/siralexandr/lab2/MainActivity.java
+++ app/src/main/java/com/siralexandr/lab2/MainActivity.java
@@ -1,14 +1,45 @@
 package com.siralexandr.lab2;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity  {
+    RadioGroup rg;
+    Spinner spinner;
+    Toast t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
+//
+       //
+               super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner=(Spinner)findViewById(R.id.spinner);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent,
+                                       View itemSelected, int selectedItemPosition, long selectedId) {
+
+                String[] choose = getResources().getStringArray(R.array.langs);
+                if(choose[selectedItemPosition].equals("Английский")){
+                    Locale locale = new Locale("en");
+                    Locale.setDefault(locale);
+                    Configuration configuration = new Configuration();
+                    configuration.locale = locale;
+                    getBaseContext().getResources().updateConfiguration(configuration, null);}
+            }
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
     }
+
 }
